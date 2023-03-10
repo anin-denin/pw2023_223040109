7112cd5a7ca45196536d9226ded3cc9c1bdd69ee
anin-denin <107341855+anin-denin@users.noreply.github.com>
2023-03-11 02:30:02 +0700

    Add files via upload

---
+++ pw2023_223040109/tugas/tugas4/4a.php
@@ -0,0 +1,5 @@
+<?php 
+    $words = ['epan', 'cipta', 'akan', 'u', 'asa', 'esok'];
+    echo 'M' . $words[4] . ' d' . $words[0] . 'mu' . ' di' . $words[1] . 'kan' . ' oleh apa yang kau kerj' . $words[2] . ' hari ini, ' . ' b' . $words[3] . 'kan' . ' b' . $words[5];
+?>
+
