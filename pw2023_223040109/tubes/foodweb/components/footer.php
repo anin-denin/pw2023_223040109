97db1505b506794cd177b1ca25e249be154e416e
anin-denin <107341855+anin-denin@users.noreply.github.com>
2023-06-10 23:17:30 +0700

    Add files via upload

---
+++ pw2023_223040109/tubes/foodweb/components/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Anin Denin Nadia</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
